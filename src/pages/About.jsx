32099e00054481c301a59e6c9b7f82af83b2034d
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2024-04-03 13:46:50 +0200

    about text mobile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,15 +5,13 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="md:flex justify-center p-16 bg-[#8F9779] font-nunito">
-      <div className=" flex flex-col items-center rounded-xl p-3 md:ml-10 bg-[#8F9779]">
-        <p className="text-2xl text-slate-300 font-bold pb-10 mb-10">
-          About me
-        </p>
+    <div className="md:flex justify-center p-3 md:p-16  bg-[#8F9779] font-nunito">
+      <div className=" flex flex-col items-center rounded-xl p-4 md:ml-10 bg-[#8F9779]">
+        <p className="text-2xl text-slate-300 font-bold p-5 mb-10">About me</p>
 
         <div>
-          <div className="flex justify-center items-center p-16  border-[#4D5D53] border-2 shadow-xl mb-10 ">
-            <section className="text-slate-300 text-xl  w-2/3 leading-6 ">
+          <div className="flex justify-center items-center p-4 md:p-16  border-[#4D5D53] border-2 shadow-xl mb-10 ">
+            <section className="text-slate-300 text-xl md:overflow-y-auto pt-4  leading-8 w-fit">
               My name is Sophie, i am a 32-year-old frontend developer with a
               passion for creating user-friendly and innovative digital
               experiences. <br /> My journey into the IT world began with a
